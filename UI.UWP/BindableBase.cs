ce7ff6d63d68454fef3f18adca3df1dfe59a269e
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-09 11:14:46 +0300

    Add bindable base

---
+++ UI.UWP/BindableBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.VoiceCommands;
+
+namespace DCT.TraineeTasks.HelloUWP.UI.UWP
+{
+    internal class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) 
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetAndRaise<T>(ref T original, T value, [CallerMemberName] string propertyName = null) 
+        {
+            if (!original.Equals(value))
+            {
+                return;
+            }
+
+            original = value;
+
+            this.OnPropertyChanged(propertyName);
+        }
+    }
+}
