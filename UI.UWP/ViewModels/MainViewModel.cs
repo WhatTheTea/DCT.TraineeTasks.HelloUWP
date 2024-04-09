b62d98a649e8f36c60d51dd3d7ba426b8166ec35
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-09 21:08:45 +0300

    Add ViewModels

---
+++ UI.UWP/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCT.TraineeTasks.HelloUWP.UI.UWP.ViewModels
+{
+    internal class MainViewModel : BindableBase
+    {
+        public ObservableCollection<PersonViewModel> People { get; } = new ObservableCollection<PersonViewModel>();
+    }
+}
---
+++ UI.UWP/ViewModels/PersonViewModel.cs
@@ -0,0 +1,29 @@
+﻿using DCT.TraineeTasks.HelloUWP.UI.UWP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCT.TraineeTasks.HelloUWP.UI.UWP.ViewModels
+{
+    internal class PersonViewModel : BindableBase
+    {
+        private Person Person { get; set; } = new Person();
+        public string Name => this.Person.Name;
+
+        public string FirstName
+        {
+            get => this.Person.FirstName;
+            set => this.SetAndRaise(value, this.Person, x => x.FirstName,
+                propertyNames:nameof(this.Name));
+        }
+
+        public string LastName
+        {
+            get => this.Person.LastName;
+            set => this.SetAndRaise(value, this.Person, x => x.LastName,
+                propertyNames:nameof(this.Name));
+        }
+    }
+}
