b07fe4c69b1829c5fc198a7eb4d93130af809990
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-09 20:49:05 +0300

    Change BindableBase to raise several PropertyChanged and set properties

--- UI.UWP/ViewModels/BindableBase.cs
+++ UI.UWP/ViewModels/BindableBase.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +11,7 @@ using Windows.ApplicationModel.VoiceCommands;
 
 namespace DCT.TraineeTasks.HelloUWP.UI.UWP
 {
-    internal class BindableBase : INotifyPropertyChanged
+    public class BindableBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -28,6 +30,35 @@ namespace DCT.TraineeTasks.HelloUWP.UI.UWP
             original = value;
 
             this.OnPropertyChanged(propertyName);
+        }
+
+        /// <summary>
+        /// Using setter and raises OnPropertyChanged
+        /// </summary>
+        /// <param name="setter">Action to set a property</param>
+        /// <param name="propertyName">Property to set</param>
+        protected void SetAndRaise<TTarget, TValue>(
+            TTarget target, TValue value, Expression<Func<TTarget, TValue>> selector,
+            [CallerMemberName] string propertyName = null,
+            params string[] propertyNames) 
+        {
+            var expression = (MemberExpression)selector.Body;
+            var property = (PropertyInfo)expression.Member;
+            property.SetValue(target, value);
+
+            this.OnPropertyChanged(propertyName);
+
+            this.NotifyOthers(propertyNames);
+        }
+
+        private void NotifyOthers(params string[] propertyNames)
+        {
+            if (propertyNames.Length < 1) return;
+
+            foreach (string name in propertyNames)
+            {
+                this.OnPropertyChanged(name);
+            }
         }
     }
 }
