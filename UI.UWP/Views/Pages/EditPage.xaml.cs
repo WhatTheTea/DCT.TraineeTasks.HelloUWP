4867998c2f9f8819583011cae459744a7dd1611b
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-10 20:35:53 +0300

    Add edit page

---
+++ UI.UWP/Views/Pages/EditPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file = "BlankPage1.xaml.cs" company = "Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies.All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DCT.TraineeTasks.HelloUWP.UI.UWP.Views.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class EditPage : Page
+    {
+       
+        public EditPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
