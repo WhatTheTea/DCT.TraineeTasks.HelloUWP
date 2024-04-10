5d339b090e1e037abafbefc92f3909ff6f6c572f
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-10 23:19:45 +0300

    Change C# 7.3 => latest

--- UI.UWP/Models/Person.cs
+++ UI.UWP/Models/Person.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 
-namespace DCT.TraineeTasks.HelloUWP.UI.UWP.Models
+namespace DCT.TraineeTasks.HelloUWP.UI.UWP.Models;
+
+public class Person
 {
-    public class Person
-    {
-        public string Name => $"{this.FirstName} {this.LastName}";
-        public string LastName { get; set; } = string.Empty;
-        public string FirstName { get; set; } = string.Empty;
-        public Entry[] Entries { get; set; } = Array.Empty<Entry>();
-    }
+    public string Name => $"{this.FirstName} {this.LastName}";
+    public string LastName { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public Entry[] Entries { get; set; } = Array.Empty<Entry>();
 }
--- UI.UWP/Views/Pages/EditPage.xaml.cs
+++ UI.UWP/Views/Pages/EditPage.xaml.cs
@@ -10,30 +10,29 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace DCT.TraineeTasks.HelloUWP.UI.UWP.Views.Pages
+namespace DCT.TraineeTasks.HelloUWP.UI.UWP.Views.Pages;
+
+/// <summary>
+/// An empty page that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class EditPage : Page
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class EditPage : Page
+    public PersonViewModel ViewModel { get; set; }
+   
+    public EditPage()
     {
-        public PersonViewModel ViewModel { get; set; }
-       
-        public EditPage()
-        {
-            this.InitializeComponent();
-        }
+        this.InitializeComponent();
+    }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            var viewModel = e.Parameter as PersonViewModel;
-            this.ViewModel = viewModel ?? throw new ArgumentNullException(nameof(viewModel));
-            base.OnNavigatedTo(e);
-        }
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+        var viewModel = e.Parameter as PersonViewModel;
+        this.ViewModel = viewModel ?? throw new ArgumentNullException(nameof(viewModel));
+        base.OnNavigatedTo(e);
+    }
 
-        private void BackButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(MainPage));
-        }
+    private void BackButton_Click(object sender, RoutedEventArgs e)
+    {
+        this.Frame.Navigate(typeof(MainPage));
     }
 }
