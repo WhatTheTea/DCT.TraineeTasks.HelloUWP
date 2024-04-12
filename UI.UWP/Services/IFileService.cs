a02b625055a904e72a0e113e32412d16189d5235
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-12 13:25:40 +0300

    Add file service

---
+++ UI.UWP/Services/IFileService.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file = "IFileService.cs" company = "Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies.All rights reserved.
+// </copyright>
+
+using System.Threading.Tasks;
+
+namespace DCT.TraineeTasks.HelloUWP.UI.UWP.Services;
+
+internal interface IFileService<T>
+    where T : class
+{
+    public Task SaveAsync(T data, string path);
+    public Task<T> LoadAsync(string path);
+}
---
+++ UI.UWP/Services/JsonFileService.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file = "JsonFileService.cs" company = "Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies.All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DCT.TraineeTasks.HelloUWP.UI.UWP.Services;
+
+// TODO: make it disposable to prevent Open/Close spam
+public class JsonFileService<T> : IFileService<T>
+    where T : class
+{
+    private static readonly JsonSerializerOptions s_options = new() { IncludeFields = true };
+    public async Task SaveAsync(T data, string path)
+    {
+        using FileStream file = File.Create(path);
+        await JsonSerializer.SerializeAsync(file, data, s_options)
+            .ConfigureAwait(false);
+    }
+    public async Task<T> LoadAsync(string path)
+    {
+        using FileStream file = new(path, FileMode.Open);
+        return await JsonSerializer.DeserializeAsync<T>(file, s_options)
+            .ConfigureAwait(false) ?? throw new FormatException("Invalid JSON");
+    }
+}
