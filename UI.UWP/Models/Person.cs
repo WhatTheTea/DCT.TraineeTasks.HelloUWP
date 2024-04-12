715c8653c62de856a3f339cc55ac647f9a2d266c
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-12 15:36:36 +0300

    Add copy constructor to person

--- UI.UWP/Models/Person.cs
+++ UI.UWP/Models/Person.cs
@@ -6,10 +6,17 @@ using System;
 
 namespace DCT.TraineeTasks.HelloUWP.UI.UWP.Models;
 
-public class Person
+public class Person(string firstName, string lastName)
 {
     public string Name => $"{this.FirstName} {this.LastName}";
-    public string LastName { get; set; } = string.Empty;
-    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = lastName;
+    public string FirstName { get; set; } = firstName;
     public Entry[] Entries { get; set; } = [];
+
+    public Person(Person other) : this(other.FirstName, other.LastName)
+    {
+        this.Entries = (Entry[])other.Entries.Clone();
+    }
+
+    public Person() : this(string.Empty, string.Empty) { }
 }
