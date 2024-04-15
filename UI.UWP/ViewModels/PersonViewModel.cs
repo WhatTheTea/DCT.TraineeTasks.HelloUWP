bd953f6c1901546a91adccb353603a9174ebede0
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-15 22:19:46 +0300

    Add entries management in datagrid

--- UI.UWP/ViewModels/PersonViewModel.cs
+++ UI.UWP/ViewModels/PersonViewModel.cs
@@ -13,6 +13,7 @@ namespace DCT.TraineeTasks.HelloUWP.UI.UWP.ViewModels;
 public class PersonViewModel : BindableBase
 {
     private readonly Person person;
+    private EntryViewModel? selectedEntry;
 
     public PersonViewModel() : this(new Person("Sample", "Text"))
     {
@@ -23,11 +24,19 @@ public class PersonViewModel : BindableBase
         this.person = person;
         this.Entries = new ObservableCollection<EntryViewModel>(person.Entries.Select(x => new EntryViewModel(x)));
 
-        this.DeleteEntryCommand = new RelayCommand<EntryViewModel>(x
-            => this.Entries.Remove(x));
-        this.AddEntryCommand = new RelayCommand(()
-            => this.Entries.Add(new EntryViewModel { Text = "Sample text" }));
+        this.DeleteEntryCommand = new RelayCommand<EntryViewModel?>(this.RemoveEntry);
+        this.AddEntryCommand = new RelayCommand(this.AddEntry);
         this.DeleteCommand = new RelayCommand(() => { });
+    }
+
+    public void AddEntry() => this.Entries.Add(new EntryViewModel { Text = "Sample text" });
+
+    public void RemoveEntry(EntryViewModel? entry)
+    {
+        if (entry is not null)
+        {
+            this.Entries.Remove(entry);
+        }
     }
 
     public string Name => this.person.Name;
@@ -60,4 +69,10 @@ public class PersonViewModel : BindableBase
     }
 
     public ObservableCollection<EntryViewModel> Entries { get; }
+
+    public EntryViewModel? SelectedEntry
+    {
+        get => this.selectedEntry;
+        set => this.SetAndRaise(ref this.selectedEntry, value);
+    }
 }
