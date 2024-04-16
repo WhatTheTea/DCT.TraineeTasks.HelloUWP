d5d32c36f9e36109724daa78ea33b1fff3cb3281
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-16 10:24:46 +0300

    Remove generated comments

--- UI.UWP/Views/Pages/MainPage.xaml.cs
+++ UI.UWP/Views/Pages/MainPage.xaml.cs
@@ -7,13 +7,8 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using DCT.TraineeTasks.HelloUWP.UI.UWP.ViewModels;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace DCT.TraineeTasks.HelloUWP.UI.UWP.Views.Pages;
 
-/// <summary>
-///     An empty page that can be used on its own or navigated to within a Frame.
-/// </summary>
 public sealed partial class MainPage : Page
 {
     public MainPage()
