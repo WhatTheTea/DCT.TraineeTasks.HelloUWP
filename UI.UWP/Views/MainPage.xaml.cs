d226e9d7a69b47efea047ff2ccc9e382fea515a6
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-22 14:26:41 +0300

    Add create person dialog

--- UI.UWP/Views/MainPage.xaml.cs
+++ UI.UWP/Views/MainPage.xaml.cs
@@ -2,8 +2,10 @@
 // Copyright (c) Digital Cloud Technologies.All rights reserved.
 // </copyright>
 
+using System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using DCT.TraineeTasks.HelloUWP.UI.UWP.Models;
 using DCT.TraineeTasks.HelloUWP.UI.UWP.ViewModels;
 
 namespace DCT.TraineeTasks.HelloUWP.UI.UWP.Views;
@@ -17,4 +19,14 @@ public sealed partial class MainPage : Page
     }
 
     public readonly MainViewModel ViewModel = MainViewModel.Instance;
+
+    private async void AddButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (this.AddDialog.DataContext is Person person)
+        {
+            person.FirstName = string.Empty;
+            person.LastName = string.Empty;
+            await this.AddDialog.ShowAsync();
+        }
+    }
 }
